7c901cfa0734e11c474a9b72a7cf470d1bd26e80
Benjamin Hartman <bhartman24@users.campus>
2019-09-11 15:13:12 -0500

    added all classes

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        private int nuggetCount = 6;
+        //private bool additionalNugget = true;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Chicken Nugget" , "Chicken Nugget", "Chicken Nugget" , "Chicken Nugget" , "Chicken Nugget" , "Chicken Nugget" };
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 354;
+        }
+
+        public void AddNugget()
+        {
+            nuggetCount++;
+            Ingredients.Add("Chicken Nugget");
+            Price += (0.25* nuggetCount);
+            Calories += (uint)(59 * nuggetCount);
+        }
+    }
+}
