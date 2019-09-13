896269f33a1c70c91abff33da9be395519a782b3
benjaminh1234 <benjaminh1234@yahoo.com>
2019-09-13 13:46:27 -0500

    updated test cases

---
+++ MenuTest/Entrees/PterodactylWings.cs
@@ -0,0 +1,31 @@
+﻿/* PterodactylWings.cs
+ * Author: Ben Hartman
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class PterodactylWings
+    {
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Chicken", "Wing Sauce" };
+                return ingredients;
+            }
+        }
+
+        public PterodactylWings()
+        {
+            this.Price = 7.21;
+            this.Calories = 318;
+        }
+    }
+}
